16342b4f3cf65dd9245a527f17897c0d76f2595c
Hannah Burdge <hburdge1@gmail.com>
2021-12-05 09:18:03 -0800

    completed lab

---
+++ index.js
@@ -0,0 +1,33 @@
+
+function add(a, b) {
+    let bit = a + b
+        return bit
+}
+
+function subtract(a,b) {
+    return a - b
+}
+
+function multiply(a,b) {
+    return a * b
+}
+
+function divide(a,b) {
+        return a / b
+}
+
+function increment(n) {
+    return n += 1
+}
+
+function decrement(n) {
+    return n -= 1
+}
+
+function makeInt(string) {
+    return parseInt(string, 10)
+}
+
+function preserveDecimal(string) {
+    return parseFloat(string, 10)
+}
